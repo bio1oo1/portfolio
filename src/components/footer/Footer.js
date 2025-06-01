6f270df4c2ccdafbd62403107766963cff641023
bio1oo1 <biostar1001@gmail.com>
2025-06-01 05:28:31 -0700

    reserve repair

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("© 2025 Goro, ❤️ All Rights Reserved")}
+          {emoji("© 2025 MATTHEW, ❤️ All Rights Reserved")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           {/* Theme by{" "}
